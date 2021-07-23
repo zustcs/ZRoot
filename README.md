13fcd36f3244ca965d5d535ee87f6a46d152748e
Junzzzz <0v0.git@gmail.com>
2021-07-24 02:19:09 +0800

    Remove all

---
+++ README.md
@@ -0,0 +1,7 @@
+# Zroot
+
+实验室内部后端框架
+
+**此仓库停止使用**
+
+所有内容移至实验室内网[GitLab](https://gitlab.zustcs.cn)
--- zroot-user/api/src/main/java/cn/zustcs/zroot/user/consumer/TestService.java
+++
@@ -1,9 +0,0 @@
-package cn.zustcs.zroot.user.consumer;
-
-/**
- * @author Jun
- * @date 2021/4/12.
- */
-public interface TestService {
-    String sayHello(String name);
-}
